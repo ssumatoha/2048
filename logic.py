7fe7495d40335b057d886f8bbe358c8822f6d00e
Alexandr <ssumatoha@gmail.com>
2022-04-30 15:13:25 +0300

    After writing the randomization

--- logic.py
+++ logic.py
@@ -1,22 +1,25 @@
 import random
 
 
-def prnt(x):
+def prnt(x):  # функция красивого вывода
+    print('-' * 10)
     for i in x:
         print(*i)
+    print('-' * 10)
 
 
-def search_mas(x):
+def search_mas(x):  # функция поиска нулевых значений
     list_zero = []
     for i in range(4):
         for j in range(4):
-            if mas[i][j] == 0:
+            if x[i][j] == 0:
                 list_zero.append([i, j])
     return list_zero
 
 
-def randomizing(x):
-    return random.choice(search_mas(x)), random.choice([2, 4])
+def randomizing(x):  # функция рандомного заполнения нулевых значений
+    random_elem = random.choice(search_mas(x))
+    x[random_elem[0]][random_elem[1]] = random.choice([2, 4])
 
 
 mas = [
@@ -26,7 +29,8 @@ mas = [
     [0, 0, 0, 0],
 ]
 
-
-print(randomizing(mas))
+prnt(mas)
+randomizing(mas)
+prnt(mas)
 # print(*search_mas(mas))
 # prnt(mas)

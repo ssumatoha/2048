bac6d1a38db61c43685b0948bfdfe6da85f88037
Alexandr <ssumatoha@gmail.com>
2022-05-06 17:35:19 +0300

    graphical_interface.py

--- graphical_interface.py
+++ graphical_interface.py
@@ -1,4 +1,5 @@
 import pygame
+from interface import *
 
 BLOCKS = 4
 SIZE_BLOCK = 110
@@ -7,6 +8,30 @@ WIDTH = BLOCKS * SIZE_BLOCK + (BLOCKS+1) * MARGIN
 HEIGHT = WIDTH + 110
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+
+mas = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+]
+
+mas_coord = [
+    [[65, 68], [186, 68], [307, 68], [428, 68]],
+    [[65, 187], [186, 187], [307, 187], [428, 187]],
+    [[65, 306], [186, 306], [307, 306], [428, 306]],
+    [[65, 425], [186, 425], [307, 425], [428, 425]],
+]
+
+
+def draw(scr):
+    for i in range(4):
+        for j in range(4):
+            fontObj = pygame.font.Font('freesansbold.ttf', 50)
+            textSurfaceObj = fontObj.render(f'{mas[i][j]}', True, BLACK, WHITE)
+            textRectObj = textSurfaceObj.get_rect()
+            textRectObj.center = (mas_coord[i][j][0], mas_coord[i][j][1])
+            scr.blit(textSurfaceObj, textRectObj)
 
 
 def run():
@@ -27,11 +52,9 @@ def run():
                         w = column * SIZE_BLOCK + (column + 1) * MARGIN
                         h = row * SIZE_BLOCK + (row + 1) * MARGIN
                         pygame.draw.rect(screen, WHITE, (w, h, 110, 110))
-                fontObj = pygame.font.Font('freesansbold.ttf', 50)
-                textSurfaceObj = fontObj.render('2', True, BLACK, WHITE)
-                textRectObj = textSurfaceObj.get_rect()
-                textRectObj.center = (65, 65)
-                screen.blit(textSurfaceObj, textRectObj)
+                draw(screen)
+
+                #  if event.key == pygame.K_UP:
 
 
 run()
--- interface.py
+++ interface.py
@@ -1,34 +1,29 @@
 from logic import *
 
-mas = [
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-]
 
-while True:
-    if len(search_mas_zero(mas)) == 1:
-        print('вы проиграли')
-        break
-    elif search2048(mas) == False:
-        print('вы победили')
-        break
-    randomizing(mas)
-    prnt(mas)
-    a = input('Введите команду: ')
-    if a == 'w':
-        mas = go_top(mas)
-        prnt(mas)
-    elif a == 's':
-        mas = go_bot(mas)
-        prnt(mas)
-    elif a == 'a':
-        go_left(mas)
-        prnt(mas)
-    elif a == 'd':
-        go_right(mas)
-        prnt(mas)
-    elif a == 'q':
-        print('выход')
-        break
+def game(x):
+    while True:
+        if len(search_mas_zero(x)) == 1:
+            print('вы проиграли')
+            break
+        elif search2048(x) == False:
+            print('вы победили')
+            break
+        randomizing(x)
+        prnt(x)
+        a = input('Введите команду: ')
+        if a == 'w':
+            x = go_top(x)
+            prnt(x)
+        elif a == 's':
+            x = go_bot(x)
+            prnt(x)
+        elif a == 'a':
+            go_left(x)
+            prnt(x)
+        elif a == 'd':
+            go_right(x)
+            prnt(x)
+        elif a == 'q':
+            print('выход')
+            break
